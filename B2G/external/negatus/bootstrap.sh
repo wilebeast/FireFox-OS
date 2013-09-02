5f0aae3b89204af6e5474e4432d8ec62f17ed771
Hadronization <info@hadrons.me>
2013-09-01 20:30:31 -0700

    First Build

--- B2G/external/negatus/bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export TOOLCH=toolchain
-export PATH=$TOOLCH/bin:$PATH
-
-if [ ! -d $TOOLCH ]
-then
-    $NDKPATH/build/tools/make-standalone-toolchain.sh --platform=android-9 --install-dir=$TOOLCH
-fi
